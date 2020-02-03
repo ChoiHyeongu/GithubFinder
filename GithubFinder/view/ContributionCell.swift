3db7941351c70a005af31660f66b68873a2a2998
최형우 <choihyengu@mintech.kr>
2020-02-03 16:04:03 +0900

    Add github contribution graph cell

---
+++ GithubFinder/view/ContributionCell.swift
@@ -0,0 +1,22 @@
+//
+//  ContributionCell.swift
+//  GithubFinder
+//
+//  Created by Choi on 2020/02/03.
+//  Copyright © 2020 Choi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContributionCell: View {
+  var body: some View {
+    Color(red: 235, green: 237, blue: 240)
+      .frame(width: 30, height: 30)
+  }
+}
+
+struct ContributionCell_Previews: PreviewProvider {
+  static var previews: some View {
+    ContributionCell()
+  }
+}
